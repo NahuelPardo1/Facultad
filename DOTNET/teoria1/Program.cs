efb3b1787c30ca950016fcd47a77082b4a98fc3d
Nahuel <Nahuelpardo3@gmail.com>
2025-03-20 12:50:11 -0300

    A

--- DOTNET/teoria1/Program.cs
+++ DOTNET/teoria1/Program.cs
@@ -1,1 +1,38 @@
 ﻿
+/* Ejercicio 6
+    string cadena ="";
+    Console.WriteLine("Ingrese una cadena de texto: ");
+    cadena = Console.ReadLine();
+    while(cadena != ""){
+        Console.WriteLine("La cadena ingresada tiene: " + cadena?.Length + " caracteres");
+        Console.WriteLine("Ingrese una cadena de texto: ");
+        cadena = Console.ReadLine();
+    };
+*/
+
+/* ejercicio 10
+for (int i = 1; i <= 1000; i++)
+        {
+            if (i % 17 == 0 || i % 29 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+*/
+
+/* ejercicio 12
+Console.WriteLine("Ingrese un numero: ");
+string st = Console.ReadLine();
+int num = int.Parse(st);
+Console.WriteLine("Los divisores de " + num + " son: ");
+for(int i=1;i<=num;i++){
+    if(num % i == 0){
+        Console.WriteLine(i);
+    }
+}
+*/
+int a =12;
+int b = 2;
+if ((b != 0) && (a/b > 5)) {
+    Console.WriteLine(a/b);
+}
